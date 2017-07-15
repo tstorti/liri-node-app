3a0c60eaaa2bde5d63cc7191ec23c5e0b32567be
Tony Storti <tonystorti@gmail.com>
2017-07-15 11:54:53 -0500

    added functions and allow for more flexible user input

--- liri.js
+++ liri.js
@@ -9,14 +9,69 @@ var command = process.argv[2];
 
 //This will show your last 20 tweets and when they were created at in your terminal/bash window.
 if(command === "my-tweets"){
+	var screenName = "tony_storti";
+	getTweets(screenName);
+}
+
+//this will show information related to the first song returned by spotify query for user-entered song title
+if(command === "spotify-this-song"){
+	var songTitle="";
+	for (var i = 3; i < process.argv.length; i++) {
+		// Build a string with the song title.
+		songTitle = songTitle + " " + process.argv[i];
+	}
+ 	
+ 	if (songTitle === ""){
+ 		songTitle = "Ace of Base The Sign";
+ 	}
+ 	spotifySong(songTitle);	
+}
+
+if(command === "movie-this"){
+	
+	var movieTitle="";
+
+	for (var i = 3; i < process.argv.length; i++) {
+		// Build a string with the song title.
+		movieTitle = movieTitle + " " + process.argv[i];
+	}
+	movieThis(movieTitle);
+}
+
+if(command === "do-what-it-says"){
+
+	var content;
+	file.readFile('./random.txt', 'utf8', function read(err, data) {
+	    if (err) {
+	        throw err;
+	    }
+	    content = data;
+	    //console.log(content);
+	    newCommand = content.split(",")[0];
+	    value = content.split(",")[1];
+	    
+	    if (newCommand ==="spotify-this-song"){
+	    	spotifySong(value);
+	    }
+	    else if (newCommand ==="movie-this"){
+	    	movieThis(value);
+	    }
+	    else if(newCommand ==="my-tweets"){
+	    	getTweets(value);
+	    }
+	});
+}
+
+function getTweets(username){
+
 	var client = new Twitter({
-	  consumer_key: keys.twitterKeys.consumer_key,
-	  consumer_secret: keys.twitterKeys.consumer_secret,
-	  access_token_key: keys.twitterKeys.access_token_key,
-	  access_token_secret: keys.twitterKeys.access_token_secret,
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret,
 	});
 	 
-	var params = {screen_name: 'tony_storti'};
+	var params = {screen_name: username};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
 	    	console.log("---------tony_storti tweets--------");
@@ -27,42 +82,35 @@ if(command === "my-tweets"){
 	    	}
 	  	}
 	});	
-}
+};
 
-//this will show information related to the first song returned by spotify query for user-entered song title
-if(command === "spotify-this-song"){
-	var songTitle = process.argv[3];
- 	
- 	if (songTitle === undefined){
- 		songTitle = "Ace of Base The Sign";
- 	}
+function spotifySong(title){
 
-	var spotify = new Spotify({
-	  id: keys.spotifyKeys.client_ID,
-	  secret: keys.spotifyKeys.client_secret,
+var spotify = new Spotify({
+		id: keys.spotifyKeys.client_ID,
+		secret: keys.spotifyKeys.client_secret,
 	});
 	 
 	spotify
-	  .search({ type: 'track', query: songTitle })
-	  .then(function(response) {
-	    //artist name
-	    console.log(response.tracks.items[0].artists[0].name);
-	    //track name
-	    console.log(response.tracks.items[0].name);
-	    //preview link
-	    console.log(response.tracks.items[0].external_urls.spotify);
-	    //album
-	    console.log(response.tracks.items[0].album.name);
+		.search({ type: 'track', query: title })
+		.then(function(response) {
+			//artist name
+			console.log(response.tracks.items[0].artists[0].name);
+			//track name
+			console.log(response.tracks.items[0].name);
+			//preview link
+			console.log(response.tracks.items[0].external_urls.spotify);
+			//album
+			console.log(response.tracks.items[0].album.name);
+		})
+		.catch(function(err) {
+			console.log(error);
+		});
+};
 
-	  })
-	  .catch(function(err) {
-	    console.log(error);
-	  });
-}
-if(command === "movie-this"){
+function movieThis(title){
 	
-	var movieTitle = process.argv[3];
-	var omdbURL = "http://www.omdbapi.com/?apikey="+keys.omdbKeys.api_key+"&t="+movieTitle;
+	var omdbURL = "http://www.omdbapi.com/?apikey="+keys.omdbKeys.api_key+"&t="+title;
 
 	request(omdbURL, function (error, response, body) {
 		 
@@ -84,44 +132,6 @@ if(command === "movie-this"){
 			console.log("error:", error);
 		}
 	});
-}
-if(command === "do-what-it-says"){
 
-	var content;
-	
-	file.readFile('./random.txt', 'utf8', function read(err, data) {
-	    if (err) {
-	        throw err;
-	    }
-	    content = data;
-
-	    console.log(content);
-	    command = content.split(",")[0];
-	    songTitle = content.split(",")[1];
-
-	    var spotify = new Spotify({
-			id: keys.spotifyKeys.client_ID,
-			secret: keys.spotifyKeys.client_secret,
-		});
-
-	    spotify
-			.search({ type: 'track', query: songTitle })
-			.then(function(response) {
-		    //artist name
-		    console.log(response.tracks.items[0].artists[0].name);
-		    //track name
-		    console.log(response.tracks.items[0].name);
-		    //preview link
-		    console.log(response.tracks.items[0].external_urls.spotify);
-		    //album
-		    console.log(response.tracks.items[0].album.name);
-
-		})
-		.catch(function(err) {
-			console.log(error);
-		});
-	});
-}
-
-
+};
 
